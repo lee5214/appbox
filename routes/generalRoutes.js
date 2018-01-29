dfdf8fce8710079383cdaa2d891dad61b090e766
congli <admin@lee5214.com>
2018-01-28 22:21:57 -0800

    17

--- routes/generalRoutes.js
+++ routes/generalRoutes.js
@@ -1,7 +1,7 @@
 module.exports = (app) => {
-	app.get ('/', (req, res) => {
-		res.send ('welcome');
-	});
+	// app.get ('/', (req, res) => {
+	// 	res.send ('welcome');
+	// });
 	app.post('/api/validateCityName', (req, res) => {
 
 	})
