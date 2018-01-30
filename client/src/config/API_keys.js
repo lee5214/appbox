725f399713d4722dd20c8dc0942c27f295a8c3d2
congli <admin@lee5214.com>
2018-01-29 18:59:40 -0800

    prodKeys fix

--- client/src/config/API_keys.js
+++ client/src/config/API_keys.js
@@ -1,11 +1,19 @@
-import prodKeys from './prodKeys';
-import devKeys from './devKeys';
+// import prodKeys from './prodKeys';
+// import devKeys from './devKeys';
 
 let keys;
 if (process.env.NODE_ENV === 'production') {
-	keys = prodKeys
+	keys = {
+		'OpenWeatherMap_Key' : process.env.OWM_API_KEY,
+		'NYT_Key' : process.env.NYT_API_KEY,
+		'GoogleGlobalAPI_Key' : process.env.GOOGLE_GLOBAL_API_KEY,
+	};
 } else {
-	keys = devKeys
+	keys = {
+		'OpenWeatherMap_Key' : '58bc6bdbc6285ac512cb870c3a126018',
+		'NYT_Key' : '7b3752510c8d426d9182b1beb2f507e3',
+		'GoogleGlobalAPI_Key' : 'AIzaSyCLCYRWrzT5cfMKwnSbKNYIBcZKw14dRwM',
+	};
 }
+
 export default keys;
-// export default process.env.NODE_ENV === 'production'?prodKeys:devKeys;
