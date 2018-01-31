35b969f1fe69ef94e7742dad926b2234041de007
congli <admin@lee5214.com>
2018-01-31 00:21:50 -0800

    backend fix stage8

--- bin/start.js
+++ bin/start.js
@@ -20,7 +20,7 @@ console.log('port:',PORT)
 if (process.env.NODE_ENV === 'production') {
 	app.use (Express.static('client/build'));
 	app.get ('*', (req, res) => {
-		res.sendFile (path.resolve (__dirname, 'client', 'build', 'index.html'));
+		res.sendFile (path.resolve (__dirname, 'client/build/index.html'));
 	});
 }
 
