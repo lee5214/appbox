a84989691d63034b47a7d015632a8a28095372b3
congli <admin@lee5214.com>
2018-01-30 18:22:12 -0800

    backend fix3

--- bin/start.js
+++ bin/start.js
@@ -72,8 +72,8 @@ require ('../routes/generalRoutes') (app);
 
 if (process.env.NODE_ENV === 'production') {
 	app.use (Express.static('client/build'));
-	app.get ('*', (req, res) => {
-		res.sendFile (path.resolve (__dirname, 'client', 'build', 'index.html'));
-	});
+	// app.get ('*', (req, res) => {
+	// 	res.sendFile (path.resolve (__dirname, 'client', 'build', 'index.html'));
+	// });
 }
 
