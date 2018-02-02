d5a33c26ba00756033bb13715b37787a809c68b3
congli <admin@lee5214.com>
2018-02-01 18:10:26 -0800

    embed https fix, deploy succeed

--- client/src/views/City/components/ForecastEmbed.js
+++ client/src/views/City/components/ForecastEmbed.js
@@ -8,7 +8,7 @@ export default class ForecastEmbed extends Component {
 			// TODO need to add a mask layer to fix the mouse over bug in 3d mode
 			<iframe title={ 'forecastEmbed' } className={ 'aaaa' } width={ '100%' } height={ '250px' }
 			        frameBorder={ '0' }
-			        src={ `http://forecast.io/embed/#lat=${lat}&lon=${lon}&color=#20a8d8&font={''}&units=us` }/>
+			        src={ `https://forecast.io/embed/#lat=${lat}&lon=${lon}&color=#20a8d8&font={''}&units=us` }/>
 		);
 	}
 }
