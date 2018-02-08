2681d870f6e46117d74385bb9d52770b1c26d948
congli <admin@lee5214.com>
2018-02-07 20:37:28 -0800

    correct init.js

--- client/config/init.js
+++ client/config/init.js
@@ -1,18 +1,17 @@
-(function () {
-  let bodyElement = document.querySelector('body')
-  bodyElement.classList.add('loading')
+let bodyElement = document.querySelector ('body');
+bodyElement.classList.add ('loading');
 
-  document.addEventListener('readystatechange', function () {
-    if (document.readyState === 'complete') {
+document.addEventListener ('readystatechange', function () {
+	if (document.readyState === 'complete') {
 
-      let bodyElement = document.querySelector('body')
-      let loaderElement = document.querySelector('#initial-loader')
+		let bodyElement = document.querySelector ('body');
+		let loaderElement = document.querySelector ('#initial-loader');
 
-      bodyElement.classList.add('loaded')
-      setTimeout(function () {
-        bodyElement.removeChild(loaderElement)
-        bodyElement.classList.remove('loading', 'loaded')
-      }, 200)
-    }
-  })
-})()
+		bodyElement.classList.add ('loaded');
+		setTimeout (function () {
+			bodyElement.removeChild (loaderElement);
+			bodyElement.classList.remove ('loading', 'loaded');
+		}, 200);
+	}
+});
+
