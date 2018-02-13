d3961cf95b1173bb493d7b98b268ebde77a2df19
congli <admin@lee5214.com>
2018-02-12 23:09:09 -0800

    production configs

--- config/credentials/prodKey.js
+++ config/credentials/prodKey.js
@@ -1,34 +1,34 @@
 module.exports = {
-	// 'cookieKey' : 'lee5214sdfniusdnfgseiprhuigdrignfdoisgjnbldfjgafnmlvcxlnmvomoprefmioearsjvziasndfksdmg',//process.env.COOKIE_KEY,
-	// 'mongo' : {
-	// 	'mongoUri' : process.env.MONGO_URI,
-	// },
-	// 'google' : {
-	// 	'googleClientID' : process.env.GOOGLE_CLIENT_ID,
-	// 	'googleClientSecret' : process.env.GOOGLE_CLIENT_SECRET,
-	// },
-	// 'facebook' : {
-	// 	'facebookAppID' : process.env.FACEBOOK_CLIENT_ID,
-	// 	'facebookAppSecret' : process.env.FACEBOOK_CLIENT_SECRET,
-	// },
-	// 'OpenWeatherMap_Key' : process.env.OWM_API_KEY,
-	// 'NYT_Key' : process.env.NYT_API_KEY,
-	// 'GoogleGlobalAPI_Key' : process.env.GOOGLE_GLOBAL_API_KEY,
-	'cookieKey' : 'lee5214sdfniusdnfgseiprhuigdrignfdoisgjnbldfjgafnmlvcxlnmvomoprefmioearsjvziasndfksdmg',
+	'cookieKey' : process.env.COOKIE_KEY,
 	'mongo' : {
-		'mongoUri' : //'mongodb://lee5214:5214yuna@ds119078.mlab.com:19078/appbox-prod'
-			'mongodb://lee5214:5214yuna@ds129946.mlab.com:29946/react-admin',
+		'mongoUri' : process.env.MONGO_URI,
 	},
 	'google' : {
-		'googleClientID' : '317666478276-1cm68env0ihvh7duulj20vvkuldnfhj2.apps.googleusercontent.com',
-		'googleClientSecret' : '6PuAXvkIcQGvv3rtzTOnHPyw',
+		'googleClientID' : process.env.GOOGLE_CLIENT_ID,
+		'googleClientSecret' : process.env.GOOGLE_CLIENT_SECRET,
 	},
 	'facebook' : {
-		'facebookAppID' : '300125043809975',
-		'facebookAppSecret' : 'aea15c248ce03bea2d3a8b3d6644fa18',
+		'facebookAppID' : process.env.FACEBOOK_CLIENT_ID,
+		'facebookAppSecret' : process.env.FACEBOOK_CLIENT_SECRET,
 	},
-	'OpenWeatherMap_Key' : '58bc6bdbc6285ac512cb870c3a126018',
-	'NYT_Key' : '7b3752510c8d426d9182b1beb2f507e3',
-	'GoogleGlobalAPI_Key' : 'AIzaSyCLCYRWrzT5cfMKwnSbKNYIBcZKw14dRwM',
+	'OpenWeatherMap_Key' : process.env.OWM_API_KEY,
+	'NYT_Key' : process.env.NYT_API_KEY,
+	'GoogleGlobalAPI_Key' : process.env.GOOGLE_GLOBAL_API_KEY,
+	// 'cookieKey' : 'lee5214sdfniusdnfgseiprhuigdrignfdoisgjnbldfjgafnmlvcxlnmvomoprefmioearsjvziasndfksdmg',
+	// 'mongo' : {
+	// 	'mongoUri' : //'mongodb://lee5214:5214yuna@ds119078.mlab.com:19078/appbox-prod'
+	// 		'mongodb://lee5214:5214yuna@ds129946.mlab.com:29946/react-admin',
+	// },
+	// 'google' : {
+	// 	'googleClientID' : '317666478276-1cm68env0ihvh7duulj20vvkuldnfhj2.apps.googleusercontent.com',
+	// 	'googleClientSecret' : '6PuAXvkIcQGvv3rtzTOnHPyw',
+	// },
+	// 'facebook' : {
+	// 	'facebookAppID' : '300125043809975',
+	// 	'facebookAppSecret' : 'aea15c248ce03bea2d3a8b3d6644fa18',
+	// },
+	// 'OpenWeatherMap_Key' : '58bc6bdbc6285ac512cb870c3a126018',
+	// 'NYT_Key' : '7b3752510c8d426d9182b1beb2f507e3',
+	// 'GoogleGlobalAPI_Key' : 'AIzaSyCLCYRWrzT5cfMKwnSbKNYIBcZKw14dRwM',
 };
 
