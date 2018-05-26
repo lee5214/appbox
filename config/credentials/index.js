4c55aa897365c0742aeabf8287622c724691b81f
lee5214 <cong-li@cong-li.com>
2018-05-25 19:38:24 -0700

    heroku deployment devKey fix

--- config/credentials/index.js
+++ config/credentials/index.js
@@ -1,11 +1,12 @@
-const prodKey = require ("./prodKey.js");
-const devKey = require ("./devKey.js")||null;
-const ciKey = require ('./ciKey.js');
+// const prodKey = require ("./prodKey.js");
+// const devKey = require ("./devKey.js")||null;
+// const ciKey = require ('./ciKey.js');
 
 if (process.env.NODE_ENV === "production") {
-  module.exports = prodKey;
+  module.exports = require ("./prodKey.js");
 }
 if (process.env.NODE_ENV === "ci") {
-  module.exports = ciKey;
+  module.exports = require ('./ciKey.js');
 }
-module.exports = devKey;
+
+module.exports = require ("./devKey.js");
