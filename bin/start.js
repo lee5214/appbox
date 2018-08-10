9805e567dc4b948f96484dd38d9d3f246db730e7
lee5214 <cong-li@cong-li.com>
2018-08-09 22:26:12 -0700

    eslint with prettier for node

--- bin/start.js
+++ bin/start.js
@@ -1,20 +1,20 @@
-const Express = require ("express");
-const app = new Express ();
-const http = require ("http");
-const mongoose = require ("mongoose");
-const passport = require ("passport");
-const keys = require ("../config/credentials");
-const bodyParser = require ("body-parser");
-const cookieSession = require ("cookie-session");
-const path = require ("path");
-const server = http.Server (app);
-const cors = require ("cors");
+const Express = require("express");
+const app = new Express();
+const http = require("http");
+const mongoose = require("mongoose");
+const passport = require("passport");
+const keys = require("../config/credentials");
+const bodyParser = require("body-parser");
+const cookieSession = require("cookie-session");
+const path = require("path");
+const server = http.Server(app);
+const cors = require("cors");
 // const socketManager = require ("../services/socketManager");
 const PORT = process.env.PORT || 4000;
-const yes = require ("yes-https");
-require ("../services/passport") ();
-require ("../models/Users_Model");
-require ("../models/SecretLinks_Model");
+const yes = require("yes-https");
+require("../services/passport")();
+require("../models/Users_Model");
+require("../models/SecretLinks_Model");
 
 // socket.io
 // io.on ('connection', socketManager);
@@ -23,41 +23,41 @@ require ("../models/SecretLinks_Model");
 /*
  * ---- mongo ----
  */
-mongoose.connect (keys.mongo.mongoUri);
+mongoose.connect(keys.mongo.mongoUri);
 
 /*
  * ---- middleware section ---- requests go through middleware before route handlers
  */
-app.use (cors ());
-app.use (bodyParser.json ());
-app.use (
-  cookieSession ({
-    maxAge : 30 * 24 * 60 * 60 * 1000,
-    keys : [keys.cookieKey],
-  }),
+app.use(cors());
+app.use(bodyParser.json());
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey]
+  })
 );
 // authRoutes
-app.use (passport.initialize ());
-app.use (passport.session ());
+app.use(passport.initialize());
+app.use(passport.session());
 
 /*
  * ---- route section ----
  */
 
-require ("../routes/authRoutes") (app);
-require ("../routes/secretLinkRoutes") (app);
-require ("../routes/cityRoutes") (app);
-require ("../routes/generalRoutes") (app);
+require("../routes/authRoutes")(app);
+require("../routes/secretLinkRoutes")(app);
+require("../routes/cityRoutes")(app);
+require("../routes/generalRoutes")(app);
 
 /*
  * ---- NODE_ENV = prod || ci ----
  */
-if (["production", "ci"].includes (process.env.NODE_ENV)) {
-  app.use (Express.static ("client/build"));
-  app.use (yes ());
-  app.get ("*", (req, res) => {
-    res.sendFile (path.resolve (__dirname, "../client/build/index.html"));
+if (["production", "ci"].includes(process.env.NODE_ENV)) {
+  app.use(Express.static("client/build"));
+  app.use(yes());
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "../client/build/index.html"));
   });
 }
-server.listen (PORT);
-console.log ("node services is running on port:", PORT);
+server.listen(PORT);
+console.log("node services is running on port:", PORT);
